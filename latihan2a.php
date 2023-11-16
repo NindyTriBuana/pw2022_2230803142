e0fc8af993f619089ea91a707b272761233e2f94
NindyTriBuana <145657246+NindyTriBuana@users.noreply.github.com>
2023-11-16 20:42:52 +0700

    Add files via upload

---
+++ latihan2a.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta charset="UTF-8">
+
+title Modul 2 - Latihan 1</titles
+
+cbody's ctsole border="1" cellpadding="5" cellspacing="
+
+<th>Kolom 1</ths
+
+<th Kolom 4</th>
+
+</tra
+
+<!-- TULIS SCRIPT PHP DIBAWAH BARIS INI -
+
+tables </body>=
